524ef8abfb37fb586612989e560c0466ba21d8e5
Pietro Dossi <pietro.dossi@gmail.com>
2026-07-29 18:28:24 +0200

    Ordini.API - Log su lettura Dapper

--- Ordini.Api/Domains/Repositories/Dapper/OrdineRepositoryReader.cs
+++ Ordini.Api/Domains/Repositories/Dapper/OrdineRepositoryReader.cs
@@ -1,6 +1,6 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
-using Ordini.Api.Configurations;
+using Ordini.Contracts;
 using Ordini.Contracts.Models;
 
 namespace Ordini.Api.Domains.Repositories.Dapper
@@ -9,9 +9,12 @@ namespace Ordini.Api.Domains.Repositories.Dapper
     {
         private readonly string _connectionString;
 
-        public OrdineRepositoryReader(IConfiguration configuration)
+        private readonly ILogger<OrdineRepositoryReader> _logger;
+
+        public OrdineRepositoryReader(IConfiguration configuration, ILogger<OrdineRepositoryReader> logger)
         {
             _connectionString = configuration.GetConnectionString(PARAMETRI_GLOBALI.CONNESSIONE_DB.MAIN_R)!;
+            _logger = logger;
         }
 
 
@@ -22,21 +25,33 @@ namespace Ordini.Api.Domains.Repositories.Dapper
         /// <returns></returns>
         public async Task<Ordine?> GetOrdineByIdAsync(string id)
         {
+            _logger.LogInformation("GetOrdineByIdAsync - IdOrdine:{0}", id);
+            if (id.Trim() == string.Empty)
+            {
+                _logger.LogError("GetOrdineByIdAsync - Parametro non passato");
+                throw new InvalidDataException("Parametro non passato");
+            }
 
-            if (id.Trim() == string.Empty) throw new InvalidDataException("Parametro non passato");
 
-            if (_connectionString.Trim() == string.Empty) throw new InvalidOperationException("Connessione non presente");
+            if (_connectionString.Trim() == string.Empty)
+            {
+                _logger.LogError("GetOrdineByIdAsync - Connessione non presente");
+                throw new InvalidOperationException("Connessione non presente");
+            }
 
+            _logger.LogInformation("GetOrdineByIdAsync - Apertura connessione DB Dapper");
 
             using var sqlconnection = new SqlConnection(_connectionString);
             await sqlconnection.OpenAsync();
 
             string sqlOrdine = @"select * from Ordini where id = @id";
+            _logger.LogInformation("GetOrdineByIdAsync - Query Get singolo ordine: {0}", sqlOrdine);
             Ordine o = await sqlconnection.QuerySingleOrDefaultAsync<Ordine>(sqlOrdine, new { id });
 
             if (o != null)
             {
                 string sqlDettaglioOrdine = @"select * from DettagliOrdine where IdOrdine = @id";
+                _logger.LogInformation("GetOrdineByIdAsync - Query Get dettaglio ordine: {0}", sqlDettaglioOrdine);
                 IEnumerable<DettaglioOrdine> elencoDettagli = await sqlconnection.QueryAsync<DettaglioOrdine>(sqlDettaglioOrdine, new { id });
 
                 o.Prodotti.AddRange(elencoDettagli);
@@ -44,7 +59,7 @@ namespace Ordini.Api.Domains.Repositories.Dapper
 
                 return o;
             }
-
+            _logger.LogError("GetOrdineByIdAsync - Ordine non trovato nel database");
             throw new DirectoryNotFoundException($"L'ordine con ID {id} non è stato trovato.");
 
         }
