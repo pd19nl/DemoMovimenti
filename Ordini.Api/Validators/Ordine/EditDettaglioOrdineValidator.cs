3cf6061d4062f8b726453392ba9cda161cfebb55
Pietro Dossi <pietro.dossi@gmail.com>
2026-07-29 16:48:25 +0200

    validazione client modifica ordine

---
+++ Ordini.Api/Validators/Ordine/EditDettaglioOrdineValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Ordini.ApplicationAPI.Models.DTOs.Ordine.Modifica;
+
+namespace Ordini.Api.Validators.Ordine
+{
+    public class EditDettaglioOrdineValidator : AbstractValidator<EditDettaglioOrdineDTO>
+    {
+
+        public EditDettaglioOrdineValidator()
+        {
+            RuleFor(o => o.CodiceArticolo)
+                .NotEmpty()
+                .WithMessage("Il Codice Articolo è obbligatorio")
+                .MaximumLength(10).WithMessage("Lunghezza Massima Codice Articolo 10");
+
+            RuleFor(o => o.Qta)
+                .GreaterThan(0)
+                .WithMessage("La quantità minima è 1");
+
+            RuleFor(o => o.Prezzo)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("il prezzo non può essere negativo");
+
+            RuleFor(o => o.Id)
+                .GreaterThan(0)
+                .WithMessage("Codice Id non valido");
+        }
+    }
+}
---
+++ Ordini.Api/Validators/Ordine/EditOrdineValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Ordini.ApplicationAPI.Models.DTOs.Ordine.Modifica;
+
+namespace Ordini.Api.Validators.Ordine
+{
+    public class EditOrdineValidator : AbstractValidator<EditOrdineDTO>
+    {
+        public EditOrdineValidator()
+        {
+            RuleFor(o => o.Id)
+                .NotEmpty().WithMessage("Il Codice Ordine è obbligatorio")
+                .MaximumLength(10).WithMessage("Lunghezza Massima Codice Ordine 10");
+
+
+            RuleFor(o => o.Prodotti)
+               .NotEmpty()
+                .WithMessage("L'ordine deve contenere almeno un articolo");
+
+            RuleForEach(o => o.Prodotti).SetValidator(new EditDettaglioOrdineValidator());
+        }
+    }
+}
