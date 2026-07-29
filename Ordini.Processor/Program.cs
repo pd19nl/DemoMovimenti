709bbc8b18bf3cd10e16591cf227df7a24e2aea0
Pietro Dossi <pietro.dossi@gmail.com>
2026-07-29 18:12:07 +0200

    Ordine.Processor - Configurazione

--- Ordini.Processor/Program.cs
+++ Ordini.Processor/Program.cs
@@ -1,6 +1,53 @@
 using Ordini.Processor;
+using RabbitMQ.Client;
+using Serilog;
+using System.Reflection;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+//  --- configurazione dei servizi ---
+var configuration = builder.Configuration;
+
+// =======================================================================================
+//configurazione RabbitMQ
+// come servizio singleton
+builder.Services.AddSingleton(r =>
+{
+    var factory = new ConnectionFactory()
+    {
+        HostName = configuration["RabbitMQ:HostName"],
+        UserName = configuration["RabbitMQ:UserName"],
+        Password = configuration["RabbitMQ:Password"],
+        DispatchConsumersAsync = true
+    };
+
+    return factory.CreateConnection();
+});
+//registrazione canale RabbitMQ come servizio Scoped
+builder.Services.AddScoped(r =>
+{
+    var connection = r.GetRequiredService<IConnection>();
+    return connection.CreateModel();
+});
+
+
+
+// =======================================================================================
+// configurazione Serilog
+var appName = Assembly.GetEntryAssembly()?.GetName().Name ?? "Ordini.Worker";
+builder.Logging.ClearProviders();
+
+//lettura appsettings
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.WithProperty("Application", appName)
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
+
+
+
+//registrazione del processo in background
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
