14ae46891bd5a4bb9988e85fd299bce4d9e5341b
Pietro Dossi <pietro.dossi@gmail.com>
2026-08-02 15:08:20 +0200

    Pagamenti.Process - Configurazione

--- Pagamenti.Processor/Program.cs
+++ Pagamenti.Processor/Program.cs
@@ -1,7 +1,7 @@
-using Pagamenti.Processor;
+using Pagamenti.Processor.Domains;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddHostedService<WorkerPagamenti>();
 
 var host = builder.Build();
 host.Run();
