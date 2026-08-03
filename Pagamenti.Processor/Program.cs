c96417eb9a9f415230e9740075dd511d7ea5c19c
Pietro Dossi <pietro.dossi@gmail.com>
2026-08-03 09:43:59 +0200

    Pagamenti.Worker - Configurazione

--- Pagamenti.Processor/Program.cs
+++ Pagamenti.Processor/Program.cs
@@ -1,7 +1,83 @@
 using Pagamenti.Processor.Domains;
+using Pagamenti.Processor.Domains.Repositories.Dapper;
+using RabbitMQ.Client;
+using Serilog;
+using System.Reflection;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+//  --- configurazione dei servizi ---
+var configuration = builder.Configuration;
+
+// =======================================================================================
+//configurazione RabbitMQ
+// come servizio singleton
+builder.Services.AddSingleton(r =>
+{
+    var factory = new ConnectionFactory()
+    {
+        HostName = configuration["RabbitMQ:HostName"],
+        UserName = configuration["RabbitMQ:UserName"],
+        Password = configuration["RabbitMQ:Password"],
+        DispatchConsumersAsync = true
+    };
+
+    return factory.CreateConnection();
+});
+//registrazione canale RabbitMQ come servizio Scoped
+builder.Services.AddScoped(r =>
+{
+    var connection = r.GetRequiredService<IConnection>();
+    return connection.CreateModel();
+});
+
+
+
+// =======================================================================================
+// configurazione Serilog
+var appName = Assembly.GetEntryAssembly()?.GetName().Name ?? "Pagamenti.Worker";
+builder.Logging.ClearProviders();
+
+//lettura appsettings
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .Enrich.WithProperty("Application", appName)
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
+
+
+// =======================================================================================
+//  aggiunta servizio di elaborazione ordini
+builder.Services.AddScoped<PagamentoRepositoryCRUD>();
+
+// =======================================================================================
+//registrazione del processo in background
 builder.Services.AddHostedService<WorkerPagamenti>();
 
+
+
+// =======================================================================================
+//
 var host = builder.Build();
-host.Run();
+
+
+//AVVIO CON GESTIONE DELLE ECCEZIONI
+//host.Run();
+try
+{
+    Log.Information("Worker [{application}] avviato con successo",
+        Assembly.GetEntryAssembly()?.GetName().Name);
+    host.Run();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex,
+              "Worker [{application}] terminato in modo anomalo",
+                Assembly.GetEntryAssembly()?.GetName().Name);
+}
+finally
+{
+    Log.CloseAndFlush();
+}
